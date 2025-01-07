c5d95ca15c4777ee996fe6e6cac8d8020079a356
chengj <chen.gj@qq.com>
2025-01-07 21:18:59 +0800

    gw test

--- config/gw-auth.php
+++ config/gw-auth.php
@@ -5,10 +5,18 @@ return [
 
     'gateways' => [
         'default' => [
+            // 网关分配的配置，用于校验来自其他应用的请求是否合法
             'private-request' => [
                 'app' => env('GW_AUTH_PRIVATE_APP'),
                 'ticket' => env('GW_AUTH_PRIVATE_TICKET'),
                 'ignore-check' => env('APP_DEBUG', false),  // 是否忽略校验，缺省是false
+            ],
+
+            // 服务发现配置，用于自动获取其他服务的配置
+            'service-discovery' => [
+                'baseUrl' => env('GW_AUTH_SERVICE_DISCOVERY_BASE_URL'),
+                'app' => env('GW_AUTH_SERVICE_DISCOVERY_APP'),
+                'ticket' => env('GW_AUTH_SERVICE_DISCOVERY_TICKET'),
             ],
         ],
 
--- config/gw.php
+++ config/gw.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    'config_file' => env('GW_CONFIG_FILE', base_path('gw.yml')),
+    'config_file' => env('GW_CONFIG_FILE') ? base_path(env('GW_CONFIG_FILE', 'gw.php')) : null,
+
+    'private_request' => [
+        'app' => env('GW_PRIVATE_APP'),
+        'ticket' => env('GW_PRIVATE_TICKET'),
+    ],
 ];
