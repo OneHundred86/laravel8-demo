303d9ea723e154d2e2752c05bebdb5a228e3bab3
chengj <chen.gj@qq.com>
2025-06-11 15:03:36 +0800

    验证码

---
+++ app/Console/Commands/Captcha/TestCaptcha.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands\Captcha;
+
+use Illuminate\Console\Command;
+use Oh86\Captcha\Facades\Captcha;
+
+class TestCaptcha extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'captcha:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // $this->defaultDemo();
+        // $this->imageDemo();
+        $this->tencentCloudDemo();
+
+        return 0;
+    }
+
+    public function defaultDemo()
+    {
+        $capt = Captcha::acquire();
+        var_dump($capt);
+
+        $result = Captcha::verify(['key' => $capt['key'], 'value' => '1234']);
+        var_dump($result);
+    }
+
+    public function imageDemo()
+    {
+        $capt = Captcha::driver('image')->acquire();
+        var_dump($capt);
+
+        $result = Captcha::driver('image')->verify(['key' => $capt['key'], 'value' => '1234']);
+        var_dump($result);
+    }
+
+    public function tencentCloudDemo()
+    {
+        // $capt = Captcha::driver('tencentCloud')->acquire();
+        // var_dump($capt);
+
+        $result = Captcha::driver('tencentCloud')->verify(['ticket' => 'your_ticket_here', 'randStr' => 'your_rand_str_here', 'userIp' => 'your_user_ip_here']);
+        var_dump($result);
+    }
+}
